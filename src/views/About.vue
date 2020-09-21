c0dea8d878f23b904c4198b5c116776ce3dd6b50
Bagusth15 <bagustri15@gmail.com>
2020-09-21 21:37:49 +0700

    first commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,77 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <b-container>
+      <b-form @submit.prevent="onSubmit" @reset.prevent="onReset">
+        <b-form-group id="input-group-1" label="Username:" label-for="input-1">
+          <b-form-input
+            id="input-1"
+            required
+            placeholder="Enter username"
+          ></b-form-input>
+        </b-form-group>
+
+        <b-form-group
+          id="input-group-2"
+          label="Email address:"
+          label-for="input-2"
+          description="We'll never share your email with anyone else."
+        >
+          <b-form-input
+            id="input-2"
+            type="email"
+            required
+            placeholder="Enter email"
+          ></b-form-input>
+        </b-form-group>
+
+        <b-form-group
+          id="input-group-3"
+          label="Your Password:"
+          label-for="input-3"
+        >
+          <b-form-input
+            id="input-3"
+            type="password"
+            required
+            placeholder="Enter password"
+          ></b-form-input>
+        </b-form-group>
+
+        <b-form-group
+          id="input-group-3"
+          label="Select Room:"
+          label-for="input-3"
+        >
+          <b-form-select class="mb-3" v-model="form.room">
+            <b-form-select-option :value="null"
+              >Please select Room</b-form-select-option
+            >
+            <b-form-select-option value="html">HTML</b-form-select-option>
+            <b-form-select-option value="css">CSS</b-form-select-option>
+            <b-form-select-option value="js">JS</b-form-select-option>
+          </b-form-select>
+        </b-form-group>
+
+        <b-button type="reset" variant="danger">Reset</b-button>
+        <b-button type="submit" variant="primary">Submit</b-button>
+      </b-form>
+    </b-container>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'Login',
+  data: () => ({
+    form: {
+      room: null
+    }
+  })
+}
+</script>
+
+<style scoped>
+.about {
+  text-align: left;
+}
+</style>
---
+++ src/views/Chat.vue
@@ -0,0 +1,108 @@
+<template>
+  <div class="chat">
+    <h2>Chat App</h2>
+    <div class="chat-window">
+      <div class="output">
+        <p v-if="typing">
+          <em>{{ typing }} is typing a message...</em>
+        </p>
+        <p><strong>Budi : </strong>Hai Semua</p>
+      </div>
+    </div>
+    <input
+      class="message"
+      type="text"
+      v-model="message"
+      placeholder="Message"
+    />
+    <button class="send">Send</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Chat',
+  data() {
+    return {
+      message: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+.chat {
+  max-width: 600px;
+  margin: 30px auto;
+  border: 1px solid #ddd;
+  box-shadow: 1px 3px 5px rgba(0, 0, 0, 0.05);
+  border-radius: 2px;
+}
+
+h2 {
+  font-size: 18px;
+  padding: 10px 20px;
+  color: #575ed8;
+}
+
+.mario-chat {
+  max-width: 600px;
+  margin: 30px auto;
+  border: 1px solid #ddd;
+  box-shadow: 1px 3px 5px rgba(0, 0, 0, 0.05);
+  border-radius: 2px;
+}
+
+.chat-window {
+  height: 400px;
+  overflow: auto;
+  background: #f9f9f9;
+}
+
+.output p {
+  text-align: left;
+  padding: 14px 0px;
+  margin: 0 20px;
+  border-bottom: 1px solid #e9e9e9;
+  color: #555;
+}
+
+.feedback p {
+  color: #aaa;
+  padding: 14px 0px;
+  margin: 0 20px;
+}
+
+.output strong {
+  color: #575ed8;
+}
+
+label {
+  box-sizing: border-box;
+  display: block;
+  padding: 10px 20px;
+}
+
+input {
+  padding: 10px 20px;
+  box-sizing: border-box;
+  background: #eee;
+  border: 0;
+  display: block;
+  width: 100%;
+  background: #fff;
+  border-bottom: 1px solid #eee;
+  font-family: Nunito;
+  font-size: 16px;
+}
+
+button {
+  background: #575ed8;
+  color: #fff;
+  font-size: 18px;
+  border: 0;
+  padding: 12px 0;
+  width: 100%;
+  border-radius: 0 0 2px 2px;
+}
+</style>
